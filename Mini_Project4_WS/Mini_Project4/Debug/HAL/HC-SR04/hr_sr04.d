d93c7bc953ebcc2a429a1c2ab4bedf579607ce02
Youssif Hossam <Yousef408@icloud.com>
2023-10-09 17:55:49 +0300

    first commit

---
+++ Mini_Project4_WS/Mini_Project4/Debug/HAL/HC-SR04/hr_sr04.d
@@ -0,0 +1,18 @@
+HAL/HC-SR04/hr_sr04.o HAL/HC-SR04/hr_sr04.o: ../HAL/HC-SR04/hr_sr04.c \
+  ../HAL/HC-SR04/hr_sr04.h ../HAL/HC-SR04/../../LIB/std_types.h \
+  ../HAL/HC-SR04/../../MCAL/ICU/icu.h \
+  ../HAL/HC-SR04/../../MCAL/ICU/../../LIB/std_types.h \
+  ../HAL/HC-SR04/../../MCAL/GPIO/gpio.h \
+  ../HAL/HC-SR04/../../MCAL/GPIO/../../LIB/std_types.h
+
+../HAL/HC-SR04/hr_sr04.h:
+
+../HAL/HC-SR04/../../LIB/std_types.h:
+
+../HAL/HC-SR04/../../MCAL/ICU/icu.h:
+
+../HAL/HC-SR04/../../MCAL/ICU/../../LIB/std_types.h:
+
+../HAL/HC-SR04/../../MCAL/GPIO/gpio.h:
+
+../HAL/HC-SR04/../../MCAL/GPIO/../../LIB/std_types.h:
---
+++ Mini_Project4_WS/Mini_Project4/Debug/app.d
@@ -0,0 +1,13 @@
+app.o app.o: ../app.c ../HAL/LCD/lcd.h ../HAL/LCD/../../LIB/std_types.h \
+  ../HAL/HC-SR04/hr_sr04.h ../HAL/HC-SR04/../../LIB/std_types.h \
+  ../LIB/std_types.h
+
+../HAL/LCD/lcd.h:
+
+../HAL/LCD/../../LIB/std_types.h:
+
+../HAL/HC-SR04/hr_sr04.h:
+
+../HAL/HC-SR04/../../LIB/std_types.h:
+
+../LIB/std_types.h:
